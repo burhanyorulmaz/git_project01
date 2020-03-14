43d76c7283b423edf04c18afd2af4ff6ab7623e8
Burhan Yorulmaz <Burhanyorulmaz02@gmail.com>
2020-03-14 22:00:59 +0100

    Last message

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -4,13 +4,12 @@ public class NextClass {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
 		System.out.println("Hello world!");
 
-=======
+
 		System.out.println("Hello dear SDETS");
 		
->>>>>>> 03ca1e6fe90357322f495cbd8dbe20d4d7611ba7
+
 	}
 
 }
